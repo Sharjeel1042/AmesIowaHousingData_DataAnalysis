bc1b2092a5968b807f1f253de756263d48f1d947
Syed Muhammad Sharjeel Farsheed <FA23-BCS-094@ISBSTUDENT.COMSATS.EDU.PK>
2025-08-25 20:40:19 +0400

    Initial Commit

---
+++ barChart_Neighborhood_VS_salePrice/barChart_Neighborhood_VS_salePrice.py
@@ -0,0 +1,24 @@
+from main import df_cleaned
+import matplotlib.pyplot as plt
+import pandas as pd
+
+grp=df_cleaned.groupby('neighborhood')['saleprice'].mean()
+
+bars=plt.bar(grp.index,grp.values,color='purple')
+plt.xticks(fontsize=6)
+plt.title('Average Sale Price vs Neighborhood',fontdict={'family':'DejaVu Sans'})
+plt.xlabel('Neighborhood',fontdict={'family':'Georgia','size':15})
+plt.ylabel('Average Sale Price/$',fontdict={'family':'Georgia','size':15})
+
+for bar in bars:
+    plt.text(
+        bar.get_x()+bar.get_width()/2,
+        bar.get_height(),
+        f'{bar.get_height():.0f}',
+        ha='center',
+        va='bottom',
+        fontsize=7
+
+    )
+plt.savefig('barChart_Neighborhood_VS_salePrice')
+plt.show()
